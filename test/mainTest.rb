f0c3cf6d55078c1e8114ed1f91c089f1b83e6204
asurielm <a.suriel@yahoo.co.uk>
2012-12-14 12:11:20 +0000

    ASM: added a new test

--- test/mainTest.rb
+++ test/mainTest.rb
@@ -14,9 +14,9 @@ class MyAppTest < Test::Unit::TestCase
     assert_equal "Making the invisible visible and sharing it", last_response.body
   end
 
-  #def test_with_params
-#    get '/meet', :name => 'Frank'
-#    assert_equal 'Hello Frank!', last_response.body
-#  end
+  def test_with_params
+    get '/game', :id => '1'
+    assert_equal '{"key1":"Game id:1","key2":"Robert wins"}', last_response.body
+  end
 
 end
